83328362dcefc01e636491989a391358b9cd9d06
Dulat <dulat.sagyntay@bk.ru>
2022-09-22 19:25:39 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,79 @@
+// "use strict";
+
+// let number = 5.6;
+
+// console.log(-4/0);
+// console.log("string" * 9);
+
+// const persone = "Alex";
+
+// const bool = true;
+
+// // console.log(something);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: "Jonh",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+// console.log(obj["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+// console.log(arr[1]);
+
+// const arr = [1, 2, 3];
+// console.log(arr[1]);
+
+// const obj = {
+//     a: 1, 
+//     b: 2,
+//     c: 3
+// };
+
+// const obj = {
+//     Anna: 500,
+//     'Alice': 800
+// };
+
+// const arrObj = {
+//     a: 'a',
+//     '1': 'b',
+//     2: 'c'
+// };
+
+// const b = 'b';
+
+// // arrObj.b = '123';
+// arrObj[b] = '123';
+
+// console.log(arrObj['b']);
+// console.log(arrObj.b);
+
+// alert('Hello');
+
+// const result = confirm('Вам 18 лет?');
+// console.log(result);
+
+// const answer = +prompt('Вам есть 18?', '18');
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "Ivan";
+
+alert(`Привет меня зовут ${user}`);
